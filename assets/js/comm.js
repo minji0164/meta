1abe69210efc3ab77b6cbb03ce8001e7a7a767b1
minji0164 <min016425@gmail.com>
2024-02-21 23:35:59 +0900

    waypoint, img hover event 추가

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,3 +1,4 @@
+//header
 let prevScrollpos = $(window).scrollTop();
 $(window).scroll(function () {
   let currentScrollPos = $(this).scrollTop();
@@ -9,8 +10,32 @@ $(window).scroll(function () {
   prevScrollpos = currentScrollPos;
 });
 
+//slide
 var swiper = new Swiper(".per-slide", {
   pagination: {
     el: ".swiper-pagination",
   },
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init();
+});
+
+//waypoint
+var counter1 = new CountUp("counter1", 0, 70, 0, 4, {
+  useEasing: true,
+  useGrouping: true,
+  separator: ",",
+});
+
+var waypoint1 = new Waypoint({
+  element: document.getElementById("waypoint1"),
+  handler: function (direction) {
+    if (direction == "up") {
+      counter1.reset();
+    } else {
+      counter1.start();
+    }
+  },
+  offset: "50%",
 });
